9cb462d1747f23654ef9243dc7c7c58c82fcd9c4
Reo Arya Yudha_E41231386 <2005jus@gmail.com>
2025-02-18 18:08:37 +0700

    update dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/',HomeController::class);
+Route::resource('dashboard',DashboardController::class);
